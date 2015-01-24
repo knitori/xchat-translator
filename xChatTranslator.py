cb6ed5bf7fa06682269f4583b9282bc32858fae3
Lars P. Søndergaard <lps@chireiden.net>
2015-01-24 17:20:14 +0100

    comply with pep8 2/2

--- xChatTranslator.py
+++ xChatTranslator.py
@@ -127,25 +127,34 @@ class Translator:
 
     CODES_SET = set(LANGUAGES.values())
 
-    def getUrl(cls, message, destLang, sourceLang=None):
+    def get_url(cls, message, dest_lang, source_lang=None):
         """
             Returns the url string to be used to translate the text.
         """
-        src = cls.findLangCode(sourceLang)
-        dest = cls.findLangCode(destLang)
+        src = cls.find_lang_code(source_lang)
+        dest = cls.find_lang_code(dest_lang)
 
         if src is None and dest is not None:
             # No source language was provided, automatically detect the language
-            return "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22" + urllib2.quote(message) + "%22%20and%20target%3D%22" + dest + "%22%3B&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="
+            return "http://query.yahooapis.com/v1/public/yql?q=select%20*" \
+                   "%20from%20google.translate%20where%20q%3D%22" + \
+                   urllib2.quote(message) + "%22%20and%20target%3D%22" +\
+                   dest + "%22%3B&format=json&diagnostics=true&env=http%" \
+                   "3A%2F%2Fdatatables.org%2Falltables.env&callback="
 
         if src is not None and dest is not None:
             # Source language was provided, don't detect the language
-            return "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20google.translate%20where%20q%3D%22" + urllib2.quote(message) + "%22%20and%20target%3D%22" + dest + "%22%20and%20source%3D%22" + src + "%22%3B&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
+            return "http://query.yahooapis.com/v1/public/yql?q=select%20*" \
+                   "%20from%20google.translate%20where%20q%3D%22"\
+                   + urllib2.quote(message) + "%22%20and%20target%3D%22"\
+                   + dest + "%22%20and%20source%3D%22" + src\
+                   + "%22%3B&format=json&env=store%3A%2F%2Fdatatables.org" \
+                     "%2Falltableswithkeys&callback="
 
         return None
-    getUrl = classmethod(getUrl)
+    get_url = classmethod(get_url)
 
-    def findLangCode(cls, language):
+    def find_lang_code(cls, language):
         """
             Checks if the specifed language is in the dict
         """
@@ -162,9 +171,9 @@ class Translator:
 
         # The language is not in the dict LANGUAGES
         return None
-    findLangCode = classmethod(findLangCode)
+    find_lang_code = classmethod(find_lang_code)
 
-    def findLangName(cls, language):
+    def find_lang_name(cls, language):
         if language is None:
             return None
 
@@ -177,13 +186,15 @@ class Translator:
             return cls.LANGUAGES_REVERSE[language]
 
         return None
-    findLangName = classmethod(findLangName)
+    find_lang_name = classmethod(find_lang_name)
 
-    def translate(cls, message, sourceLang, destLang):
+    def translate(cls, message, source_lang, dest_lang):
         """
-            Contacts the translation website via YQL to translate the message.
+            Contacts the translation website via
+            YQL to translate the message.
         """
-        url = cls.getUrl(message, sourceLang, destLang)
+        global LAST_ERROR
+        url = cls.get_url(message, source_lang, dest_lang)
 
         if url is None:
             # The Url could not be created
@@ -193,27 +204,29 @@ class Translator:
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = urllib2.urlopen(urllib2.Request(url, None, headers))
 
-        return cls.parseJsonResult(response.read())
+        return cls.parse_json_result(response.read())
     translate = classmethod(translate)
 
-    def parseJsonResult(cls, result):
+    def parse_json_result(cls, result):
         """
-            Parse the JSON returned from calling YQL to get the translated information.
+            Parse the JSON returned from calling YQL to
+            get the translated information.
         """
         data = json.loads(result)
 
-        sourceLang = data['query']['lang']
-        dataArr = data['query']['results']['json']['sentences']
+        source_lang = data['query']['lang']
+        data_arr = data['query']['results']['json']['sentences']
         translation = ""
 
-        if type(dataArr) is dict:
-            translation += dataArr['trans']
+        if type(data_arr) is dict:
+            translation += data_arr['trans']
         else:
-            for subDict in dataArr:
+            for subDict in data_arr:
                 translation += subDict['trans']
 
-        return (cls.LANGUAGES_REVERSE[sourceLang], translation.encode("utf-8"))
-    parseJsonResult = classmethod(parseJsonResult)
+        return (cls.LANGUAGES_REVERSE[source_lang],
+                translation.encode("utf-8"))
+    parse_json_result = classmethod(parse_json_result)
 
 
 class TranslatorThread(Thread):
@@ -221,7 +234,7 @@ class TranslatorThread(Thread):
         Performs the translations in threads so as not to lock up XChat.
     """
     def __init__(self, queue):
-        threading.Thread.__init__(self, target = self.run)
+        threading.Thread.__init__(self, target=self.run)
         self.queue = queue
         self.kill = False
 
@@ -234,18 +247,21 @@ class TranslatorThread(Thread):
             if self.kill or job is None:
                 break
             try:
-                context, user, srcLang, destLang, text = job
+                context, user, src_lang, dest_lang, text = job
 
-                lang, translatedText = Translator.translate(text, destLang, srcLang)
+                lang, translated_text = \
+                    Translator.translate(text, dest_lang, src_lang)
 
-                if translatedText.strip().lower() != text.strip().lower():
-                    context.emit_print("Channel Message", "[%s][%s]" %(user, lang), translatedText)
+                if translated_text.strip().lower() != text.strip().lower():
+                    context.emit_print("Channel Message",
+                                       "[%s][%s]" % (user, lang),
+                                       translated_text)
             except TranslateException, e:
-                LAST_ERROR = "[TE: %s] <%s> %s" %(e, user, text)
+                LAST_ERROR = "[TE: %s] <%s> %s" % (e, user, text)
             except urllib2.URLError, e:
-                LAST_ERROR = "[URL] %s" %e
+                LAST_ERROR = "[URL] %s" % e
             except UnicodeError, e:
-                LAST_ERROR = "[Encode: %s] <%s> %s" %(e, user, text)
+                LAST_ERROR = "[Encode: %s] <%s> %s" % (e, user, text)
 
 
 class ThreadController:
@@ -257,19 +273,20 @@ class ThreadController:
     worker.setDaemon(True)
     worker.start()
 
-    def addJob(cls, job):
+    def add_job(cls, job):
         cls.jobs.put(job)
-    addJob = classmethod(addJob)
+    add_job = classmethod(add_job)
 
 
-def translateDetectLang(word, word_eol, userdata):
+def translate_detect_lang(word, word_eol, userdata):
     """
-        Translates the message to the specified language with source language detection
+        Translates the message to the specified language
+        with source language detection
     """
-    destLang = word[1]
+    dest_lang = word[1]
     message = unicode(word_eol[2], "utf-8")
 
-    src, text = Translator.translate(word_eol[2], destLang, None)
+    src, text = Translator.translate(word_eol[2], dest_lang, None)
 
     if src is None or text is None:
         xchat.prnt("Error occurred during translation.")
@@ -278,29 +295,39 @@ def translateDetectLang(word, word_eol, userdata):
 
     return xchat.EAT_ALL
 
-xchat.hook_command("TR", translateDetectLang, help="/TR <target language> <message> - translates message into the language specified.  This auto detects the source language.  This is not threaded.")
+xchat.hook_command(
+    "TR", translate_detect_lang,
+    help="/TR <target language> <message> - translates message into the "
+         "language specified.  This auto detects the source language.  "
+         "This is not threaded.")
 
 
-def translateNoDetect(word, word_eol, userdata):
+def translate_no_detect(word, word_eol, userdata):
     """
-        Translates the message to the specified language assuming that the source language is the one specified.
+        Translates the message to the specified language assuming that
+        the source language is the one specified.
     """
-    srcLang = word[1]
-    destLang = word[2]
+    src_lang = word[1]
+    dest_lang = word[2]
     message = word_eol[3]
 
-    src, text = Translator.translate(message, destLang, srcLang)
+    src, text = Translator.translate(message, dest_lang, src_lang)
 
     if src is None or text is None:
         xchat.prnt("Error occurred during translation.")
     else:
-        xchat.prnt("Translated from " + Translator.findLangName(srcLang) + " to " + Translator.findLangName(destLang) + ": " + text)
+        xchat.prnt("Translated from " + Translator.find_lang_name(src_lang)
+                   + " to " + Translator.find_lang_name(dest_lang)
+                   + ": " + text)
     return xchat.EAT_ALL
 
-xchat.hook_command("TM", translateNoDetect, help="/TM <source_language> <target_language> <message> - translates message into the language specified.  This is not threaded.")
+xchat.hook_command(
+    "TM", translate_no_detect,
+    help="/TM <source_language> <target_language> <message> - translates "
+         "message into the language specified.  This is not threaded.")
 
 
-def addUser(word, word_eol, userdata):
+def add_user(word, word_eol, userdata):
     """
         Adds a user to the watch list to automatically translate.
     """
@@ -312,10 +339,10 @@ def addUser(word, word_eol, userdata):
     dest = DEFAULT_LANG
     src = None
 
-    if len(word) > 2 and Translator.findLangCode(word[2]) is not None:
+    if len(word) > 2 and Translator.find_lang_code(word[2]) is not None:
         dest = word[2]
 
-    if len(word) > 3 and Translator.findLangCode(word[3]) is not None:
+    if len(word) > 3 and Translator.find_lang_code(word[3]) is not None:
         src = word[3]
 
     AUTOUSER[xchat.get_info('channel') + ' ' + user.lower()] = (dest, src)
@@ -324,10 +351,16 @@ def addUser(word, word_eol, userdata):
     return xchat.EAT_ALL
 
 xchat.command('MENU ADD "$NICK/[+] AutoTranslate" "ADDTR %s"')
-xchat.hook_command("ADDTR", addUser, help = "/ADDTR <user_nick> <target_language> <source_language> - adds the user to the watch list for automatic translations.  If target_language is not specified, then the DEFAULT_LANG set will be used.  If source_language is not specified, then language detection will be used.")
+xchat.hook_command(
+    "ADDTR", add_user,
+    help="/ADDTR <user_nick> <target_language> <source_language> - adds the "
+         "user to the watch list for automatic translations.  If "
+         "target_language is not specified, then the DEFAULT_LANG set will "
+         "be used.  If source_language is not specified, then language "
+         "detection will be used.")
 
 
-def removeUser(word, word_eol, userdata):
+def remove_user(word, word_eol, userdata):
     """
         Removes a user from the watch list to automatically translate.
     """
@@ -337,55 +370,66 @@ def removeUser(word, word_eol, userdata):
 
     user = word[1]
 
-    if AUTOUSER.pop(xchat.get_info('channel') + ' ' + user.lower(), None) is not None:
-        xchat.prnt("User %s has been removed from the watch list." %user)
+    if AUTOUSER.pop(xchat.get_info('channel') + ' ' + user.lower(), None)\
+            is not None:
+        xchat.prnt("User %s has been removed from the watch list." % user)
 
     return xchat.EAT_ALL
 
 xchat.command('MENU ADD "$NICK/[-] AutoTranslate" "RMTR %s"')
-xchat.hook_command("RMTR", removeUser, help = "/RMTR <user_nick> - removes user_nick from the watch list for automatic translations.")
+xchat.hook_command(
+    "RMTR", remove_user,
+    help="/RMTR <user_nick> - removes user_nick from "
+         "the watch list for automatic translations.")
 
 
-def printWatchList(word, word_eol, userdata):
+def print_watch_list(word, word_eol, userdata):
     """
         Prints automatic translations watch list.
     """
-    users = [ key.split(' ')[1] for key in AUTOUSER.keys()]
+    users = [key.split(' ')[1] for key in AUTOUSER.keys()]
 
-    xchat.prnt("WatchList: %s" %(" ".join(users)))
+    xchat.prnt("WatchList: %s" % (" ".join(users)))
     xchat.EAT_ALL
 
-xchat.hook_command("LSUSERS", printWatchList, help = "/LSUSERS - prints out all users on the watch list for automatic translations to the screen locally.")
+xchat.hook_command(
+    "LSUSERS", print_watch_list,
+    help="/LSUSERS - prints out all users on the watch list for automatic "
+         "translations to the screen locally.")
 
 
-def readError(word, word_eol, userdata):
+def read_error(word, word_eol, userdata):
     """
         Prints out the last error.
     """
     xchat.prnt("Last error: " + LAST_ERROR)
 
-xchat.hook_command("LASTERROR", readError, help = "/LASTERROR - prints out the last error message to screen locally.")
+xchat.hook_command(
+    "LASTERROR", read_error, help="/LASTERROR - prints out the last error "
+                                  "message to screen locally.")
 
 
-def addJob(word, word_eol, userdata):
+def add_job(word, word_eol, userdata):
     """
         Adds a new translation job to the queue.
     """
     channel = xchat.get_info('channel')
     key = channel + " " + word[0].lower()
 
-    if(AUTOUSER.has_key(key)):
+    if AUTOUSER.has_key(key):
         dest, src = AUTOUSER[key]
-        ThreadController.addJob((xchat.get_context(), word[0], src, dest, word[1]))
+        ThreadController.addJob((xchat.get_context(), word[0],
+                                 src, dest, word[1]))
 
     return xchat.EAT_NONE
 
-xchat.hook_print("Channel Message", addJob)
+xchat.hook_print("Channel Message", add_job)
 
 
 def unload_translator(userdata):
     """
-        Shuts down the threads and thread controller when unloading the module.
+        Shuts down the threads and thread controller
+        when unloading the module.
     """
     ThreadController.worker.kill = True
     ThreadController.addJob(None)
